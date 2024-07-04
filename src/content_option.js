63ffe96b60e9dd3b444dda23df79ed94112b7e48
inspire1805 <inspireuplift1805@gmail.com>
2024-07-04 16:15:51 +0530

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,164 @@
+const logotext = "Prateek Sehgal";
+const meta = {
+    title: "Prateek Sehgal",
+    description: "I’m Prateek Sehgal, web developer _ Full stack devloper",
+};
+
+const introdata = {
+    title: "I’m Prateek Sehgal",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+    },
+    description: "Welcome to my web developer portfolio! I build clean, user-friendly websites and applications. Explore my work to see how I bring ideas to life with a touch of creativity and a lot of code. Let's make the web simple, stylish, and functional together. Dive in",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "About MySelf",
+    aboutme: "Full-Stack Developer with a strong background in web development, skilled in Python, Django, MySQL, JavaScript, ReactJS, and more. Proven ability in problem-solving, and collaborating effectively within teams, and adept in utilizing tools like Jenkins, JIRA, and Git for efficient development processes. Holds a Bachelor's degree in Computer Science Engineering.",
+};
+const worktimeline = [{
+        jobtitle: "FullStack Developer",
+        where: "Collegedekho",
+        date: "2021",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Django",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+];
+
+const services = [ {
+    title: "Python Development",
+    description: "Utilizing the power of Python for efficient and scalable web application development.",
+},
+{
+    title: "Django Framework",
+    description: "Building robust and secure web applications using the Django framework for rapid development.",
+},
+{
+    title: "JavaScript",
+    description: "Creating dynamic and interactive web pages with the versatility of JavaScript.",
+},
+{
+    title: "React.js",
+    description: "Developing modern, single-page applications with the declarative power of React.js.",
+},
+{
+    title: "jQuery",
+    description: "Enhancing client-side scripting with the simplicity and efficiency of jQuery.",
+},
+{
+    title: "Node.js",
+    description: "Empowering server-side development with the speed and scalability of Node.js.",
+},
+];
+
+// const dataportfolio = [{
+//         img: "https://picsum.photos/400/?grayscale",
+//         description: "The wisdom of life consists in the elimination of non-essentials.",
+//         link: "#",
+//     },
+//     {
+//         img: "https://picsum.photos/400/800/?grayscale",
+//         description: "The wisdom of life consists in the elimination of non-essentials.",
+//         link: "#",
+//     },
+//     {
+//         img: "https://picsum.photos/400/?grayscale",
+//         description: "The wisdom of life consists in the elimination of non-essentials.",
+//         link: "#",
+//     },
+//     {
+//         img: "https://picsum.photos/400/600/?grayscale",
+//         description: "The wisdom of life consists in the elimination of non-essentials.",
+//         link: "#",
+//     },
+//     {
+//         img: "https://picsum.photos/400/300/?grayscale",
+//         description: "The wisdom of life consists in the elimination of non-essentials.",
+//         link: "#",
+//     },
+//     {
+//         img: "https://picsum.photos/400/700/?grayscale",
+//         description: "The wisdom of life consists in the elimination of non-essentials.",
+//         link: "#",
+//     },
+
+//     {
+//         img: "https://picsum.photos/400/600/?grayscale",
+//         description: "The wisdom of life consists in the elimination of non-essentials.",
+//         link: "#",
+//     },
+//     {
+//         img: "https://picsum.photos/400/300/?grayscale",
+//         description: "The wisdom of life consists in the elimination of non-essentials.",
+//         link: "#",
+//     },
+//     {
+//         img: "https://picsum.photos/400/?grayscale",
+//         description: "The wisdom of life consists in the elimination of non-essentials.",
+//         link: "#",
+//     },
+//     {
+//         img: "https://picsum.photos/400/550/?grayscale",
+//         description: "The wisdom of life consists in the elimination of non-essentials.",
+//         link: "#",
+//     },
+//     {
+//         img: "https://picsum.photos/400/?grayscale",
+//         description: "The wisdom of life consists in the elimination of non-essentials.",
+//         link: "#",
+//     },
+//     {
+//         img: "https://picsum.photos/400/700/?grayscale",
+//         description: "The wisdom of life consists in the elimination of non-essentials.",
+//         link: "#",
+//     },
+// ];
+
+const contactConfig = {
+    YOUR_EMAIL: "prateeksehgal38@gmail.com",
+    YOUR_FONE: "8219963818",
+    description: "",
+    YOUR_SERVICE_ID: "service_unizl0w",
+    YOUR_TEMPLATE_ID: "template_dd10t7m",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/prateek6542",
+    linkedin: "https://www.linkedin.com/in/prateek-sehgal-8b2709194/",
+    twitter: "https://twitter.com/PrateekSehgal14",
+};
+export {
+    meta,
+    dataabout,
+    // dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
